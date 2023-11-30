5883c65120931a1ea1608e139d7ad9cffdaa155b
bermet <bermet.alibek@gmail.com>
2023-11-29 23:44:25 -0600

    check.py update

---
+++ check.py
@@ -0,0 +1,13 @@
+BOT_TOKEN = "6066026423:AAGClrZrJ1izJVOQW7uWe9a8oPZczbNYBO0"
+
+import os
+
+import telebot
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "How can I help you?")
+
+bot.infinity_polling()
